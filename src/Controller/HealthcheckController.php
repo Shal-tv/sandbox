317cc3f9d99916848611fac9890916b25b1e03df
Thomas <shaltv@protonmail.com>
2024-06-04 14:22:51 +0200

    Test

---
+++ src/Controller/HealthcheckController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class HealthcheckController
+ *
+ * @package App\Controller
+ */
+#[Route(path: '/healthcheck')]
+final class HealthcheckController extends AbstractController
+{
+
+    #[Route(path: '/ping', name: 'ping', methods: ['GET'])]
+    public function pingAction() : Response
+    {
+        return new JsonResponse('pong');
+    }
+
+}
